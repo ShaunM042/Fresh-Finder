a184a394f3114fa433a625f430524fa9ea324c0f
Shaun <shaunmathew04@outlook.com>
2025-05-18 15:28:34 -0400

    Added team_logos.py and cleaned up related imports and structure in app.py

--- app.py
+++ app.py
@@ -1,53 +1,33 @@
+# Standard Library
+import os
+from datetime import datetime, timedelta
+
+# Third-Party Libraries
+import requests
+import pandas as pd
 from dotenv import load_dotenv
-load_dotenv()
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from nba_api.stats.static import players, teams
-from nba_api.stats.endpoints import playergamelog, commonplayerinfo, playercareerstats, teamgamelog, commonteamroster, teamdetails, leaguedashteamstats
-from datetime import datetime, timedelta
-import os
-import requests
+from nba_api.stats.endpoints import (
+    playergamelog, commonplayerinfo, playercareerstats,
+    teamgamelog, commonteamroster, teamdetails, leaguedashteamstats
+)
 from nba_api.stats.library.http import NBAStatsHTTP
-import pandas as pd
 
-NBAStatsHTTP._session = requests.Session()
-NBAStatsHTTP._session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
-})
+# Application-Specific Modules
+from team_logos import TEAM_LOGOS
+
+def configure_nba_api():
+    NBAStatsHTTP._session = requests.Session()
+    NBAStatsHTTP._session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
+    })
+
+load_dotenv()
+configure_nba_api()
 
 app = Flask(__name__, template_folder='templates')
 
-TEAM_LOGOS = {
-    'ATL': 'https://a.espncdn.com/i/teamlogos/nba/500/atl.png',
-    'BOS': 'https://a.espncdn.com/i/teamlogos/nba/500/bos.png',
-    'BKN': 'https://a.espncdn.com/i/teamlogos/nba/500/bkn.png',
-    'CHA': 'https://a.espncdn.com/i/teamlogos/nba/500/cha.png',
-    'CHI': 'https://a.espncdn.com/i/teamlogos/nba/500/chi.png',
-    'CLE': 'https://a.espncdn.com/i/teamlogos/nba/500/cle.png',
-    'DAL': 'https://a.espncdn.com/i/teamlogos/nba/500/dal.png',
-    'DEN': 'https://a.espncdn.com/i/teamlogos/nba/500/den.png',
-    'DET': 'https://a.espncdn.com/i/teamlogos/nba/500/det.png',
-    'GSW': 'https://a.espncdn.com/i/teamlogos/nba/500/gsw.png',
-    'HOU': 'https://a.espncdn.com/i/teamlogos/nba/500/hou.png',
-    'IND': 'https://a.espncdn.com/i/teamlogos/nba/500/ind.png',
-    'LAC': 'https://a.espncdn.com/i/teamlogos/nba/500/lac.png',
-    'LAL': 'https://a.espncdn.com/i/teamlogos/nba/500/lal.png',
-    'MEM': 'https://a.espncdn.com/i/teamlogos/nba/500/mem.png',
-    'MIA': 'https://a.espncdn.com/i/teamlogos/nba/500/mia.png',
-    'MIL': 'https://a.espncdn.com/i/teamlogos/nba/500/mil.png',
-    'MIN': 'https://a.espncdn.com/i/teamlogos/nba/500/min.png',
-    'NOP': 'https://a.espncdn.com/i/teamlogos/nba/500/no.png',
-    'NYK': 'https://a.espncdn.com/i/teamlogos/nba/500/nyk.png',
-    'OKC': 'https://a.espncdn.com/i/teamlogos/nba/500/okc.png',
-    'ORL': 'https://a.espncdn.com/i/teamlogos/nba/500/orl.png',
-    'PHI': 'https://a.espncdn.com/i/teamlogos/nba/500/phi.png',
-    'PHX': 'https://a.espncdn.com/i/teamlogos/nba/500/phx.png',
-    'POR': 'https://a.espncdn.com/i/teamlogos/nba/500/por.png',
-    'SAC': 'https://a.espncdn.com/i/teamlogos/nba/500/sac.png',
-    'SAS': 'https://a.espncdn.com/i/teamlogos/nba/500/sas.png',
-    'TOR': 'https://a.espncdn.com/i/teamlogos/nba/500/tor.png',
-    'UTA': 'https://a.espncdn.com/i/teamlogos/nba/500/utah.png',
-    'WAS': 'https://a.espncdn.com/i/teamlogos/nba/500/was.png',
-}
 
 @app.route('/')
 def landing():
